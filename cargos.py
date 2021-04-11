04a41aa7e0cf089f6458f2c3be1549f922944687
Christian <christian.cruvinel.franca@gmail.com>
2021-04-10 22:38:51 -0300

    Adicionado cargos

---
+++ cargos.py
@@ -0,0 +1,24 @@
+from fastapi import Depends, FastAPI, HTTPException, APIRouter, status, Body
+from typing import List
+from database import db
+from bson import ObjectId
+import os
+
+COLLECTION = "cargos"
+collection = db[COLLECTION]
+
+# Define nosso router
+router = APIRouter(prefix="/cargos", tags=["Aprovações"])
+
+KEY_GERENTE = os.environ.get("KEY_GERENTE") if os.environ.get("KEY_GERENTE") is not None else "1234"
+KEY_SERVIDOR = os.environ.get("KEY_SERVIDOR") if os.environ.get("KEY_SERVIDOR") is not None else "5678"
+
+cargo_key = {0: KEY_GERENTE, 1: KEY_SERVIDOR}
+
+def checkKey(key, cargo):
+    return key == cargo_key[cargo]
+
+
+@router.get("/keycheck", summary="Validar chave")
+def post_material(key: str, cargo: int):
+    return {"valid": checkKey(key, cargo)}
--- database.py
+++ database.py
@@ -2,13 +2,15 @@ import pymongo
 from pymongo import MongoClient
 import os
 
-DATABASE_LOGIN = os.environ.get('DATABASE_LOGIN')
-DATABASE_PASSWORD = os.environ.get('DATABASE_PASSWORD')
+#DATABASE_LOGIN = os.environ.get('DATABASE_LOGIN')
+#DATABASE_PASSWORD = os.environ.get('DATABASE_PASSWORD')
 
-if not DATABASE_LOGIN or not DATABASE_PASSWORD:
-    raise Exception("No DATABASE_LOGIN or DATABASE_PASSWORD available.")
-else:
-    print("Database data available! Connecting...")
+#if not DATABASE_LOGIN or not DATABASE_PASSWORD:
+#    raise Exception("No DATABASE_LOGIN or DATABASE_PASSWORD available.")
+#else:
+#    print("Database data available! Connecting...")
+DATABASE_LOGIN = "admin"
+DATABASE_PASSWORD = "pedidosdecompra"
 
 URL = f"mongodb+srv://{DATABASE_LOGIN}:{DATABASE_PASSWORD}@cluster0.zj9tl.mongodb.net/Cluster0?"
 client = MongoClient(URL)
