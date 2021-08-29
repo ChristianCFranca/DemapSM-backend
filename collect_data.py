540c467de8748997039c43f6ebe4d1ff9d8be6e0
Christian <christian.cruvinel.franca@gmail.com>
2021-08-29 02:56:35 -0300

    Inserido botão de download

--- collect_data.py
+++ collect_data.py
@@ -1,67 +1,17 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Body
 from fastapi.responses import StreamingResponse
 
 from auth import permissions_user_role
 from cargos import RoleName
 
-from crud_pedidos import getPedidos, map_pedidos_for_compra_demap
+from crud_pedidos import map_pedidos_for_compra_demap
 
-from generate_pdf_and_sheet import stage_and_download_pdf_compras_demap
+from generate_pdf_and_sheet import stage_and_download_pdf_compras_demap, get_pdf_link_for_download
 
-from io import StringIO, BytesIO
-import pandas as pd
+from io import BytesIO
 
 # Define nosso router
 router = APIRouter(prefix="/collect-data", tags=["Pedidos de Compra"])
-
-def getCorrectDF(df):
-    new_df = pd.DataFrame(columns=df.drop('items', axis=1).rename({'quantidade': 'quantidadeDeItens'}, axis=1).columns.tolist() + 
-    ['item_id'] +
-    list(df['items'][0][0].keys())) # Dataframe vazio
-
-    j = 0
-    for i, row in df.iterrows():
-        item_id = 0
-        cols_from_original = df.iloc[i].drop('items').rename({'quantidade': 'quantidadeDeItens'}, axis=1).to_dict() # Tendo a base de repetição
-        cols_from_original['_id'] = str(cols_from_original['_id']) # Transforma aquele _id pro formato string
-        for item in row['items']: # item é um dict
-            cols_from_original.update(item) # Atualizamos todas as colunas do original com as novas colunas do item
-            item = pd.Series(cols_from_original)
-            item['item_id'] = item_id 
-
-            new_df.loc[j] = item
-
-            j += 1
-            item_id += 1
-
-    int_cols = ['statusStep', 'item_id', 'quantidadeDeItens']
-    bool_cols = ['active', 'aprovadoAssistente', 'aprovadoFiscal', 'almoxarifadoPossui']
-    float_cols = ['quantidade']
-
-    new_df[int_cols] = new_df[int_cols].astype('int64')
-    new_df[bool_cols] = new_df[bool_cols].astype('boolean')
-    new_df[float_cols] = new_df[float_cols].astype('float')
-
-    return new_df
-
-
-@router.get("/andamentos", summary="Get todos os pedidos como arquivo", 
-    dependencies=[Depends(permissions_user_role(approved_roles=[
-        RoleName.admin, RoleName.fiscal, RoleName.assistente
-        ]))])
-async def collect_andamentos():
-    all_pedidos = getPedidos()
-    pedidos_df = pd.DataFrame.from_records(all_pedidos)
-    pedidos_df = getCorrectDF(pedidos_df)
-
-    csv_file = StringIO()
-    pedidos_df.to_csv(csv_file, index=False, sep=';', encoding='latin')
-
-    return StreamingResponse(
-        iter([csv_file.getvalue()]),
-        headers={"Content-Disposition": "inline; filename=\"data.csv\""},
-        media_type='text/csv'
-    )
 
 @router.get("/compras-demap", summary="Get todos os pedidos pendentes para o DEMAP como um pdf", 
     dependencies=[Depends(permissions_user_role(approved_roles=[
@@ -89,3 +39,13 @@ def collect_compra_demap():
         iter([pdf_file.getvalue()]),
         media_type='application/pdf'
     )
+
+@router.post("/pdfs", summary="Get os links para download dos PDFs associados.",
+    dependencies=[Depends(permissions_user_role(approved_roles=[
+            RoleName.admin, RoleName.fiscal, RoleName.assistente
+            ]))])
+async def collect_pdfs(pdfs_ids: dict = Body(...)):
+    pdfs_links = dict()
+    for dept, pdf_id in pdfs_ids.items():
+        pdfs_links[dept] = get_pdf_link_for_download(pdf_id)
+    return pdfs_links
--- generate_pdf_and_sheet.py
+++ generate_pdf_and_sheet.py
@@ -47,6 +47,15 @@ def stage_pdf(json_data, departamento):
             print("\033[93mPDF:\033[0m" + f"\t  Não foi possível postar o PDF. Tentando novamente... Tentativas restantes: {counter}")
     raise HTTPException(status_code=status_code.HTTP_500_INTERNAL_SERVER_ERROR, detail="Não foi possível postar a criação do PDF.")
 
+def get_pdf_link_for_download(pdf_id):
+    response = requests.get(f"{BASE_URL}/{pdf_id}", headers=AUTH_HEADER)
+    if response.status_code == 200:
+        return response.json()['document']['download_url']
+    elif response.status_code == 401:
+        raise HTTPException(status_code=status_code.HTTP_400_BAD_REQUEST, detail="Um dos ID's passados não existe nos PDFs do banco de dados.")
+    else:
+        raise HTTPException(status_code=status_code.HTTP_500_INTERNAL_SERVER_ERROR, detail="Ocorreu um erro ao tentar coletar os PDFs.")
+
 def stage_and_download_pdf_compras_demap(json_data):
     json_data['document']['document_template_id'] = TEMPLATES_FOR_COMPRA["demap"]
     for counter in range(5, 0, -1): # 5 tentativas
